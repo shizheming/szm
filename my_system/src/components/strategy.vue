697b6a56f1da94c96c89d1079a6f276cfc764446
shape.shi <shape.shi@handeson.com>
2019-11-04 10:20:04 +0800

    x

--- my_system/src/components/strategy.vue
+++ my_system/src/components/strategy.vue
@@ -5,13 +5,46 @@
 <script>
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-import relationship from '../script/relationship';
 import strategy from '../script/strategy';
 
-function a () {
-    
+function a (j, k) {
+    return j < k;
 }
 
+function b (j, k) {
+    return j > k;
+}
+
+function c (j, k) {
+    return j === k;
+}
+
+function a1 () {
+    return '小于';
+}
+
+function b1 () {
+    return '大于';
+}
+
+function c1 () {
+    return '等于';
+}
+
+var s = strategy([
+    {
+        m: a,
+        y: a1
+    },
+    {
+        m: b,
+        y: b1
+    },
+    {
+        m: c,
+        y: c1
+    }
+]);
 
 
 
--- my_system/src/script/strategy.js
+++ my_system/src/script/strategy.js
@@ -1,3 +1,5 @@
+import relationship from './relationship';
+
 /* 
     策略
     在我看来js乃至人类语言本身有两条路，第一条不叉路，一个起点，一个终点，第二条分叉路，一个起点，分叉成n个终点，造成分叉的原因是判断语句，如果js没有判断一条路写到底会怎么样
@@ -8,9 +10,8 @@
     逻辑没有分叉会很简单，一条路笔直走到底
 */
 
-function strategy () {
-    
+function strategy (r) {
+    var g = relationship(r);
+    console.log(g, 1);
 }
-export default {
-
-};
+export default strategy;
