6bc8682716758a50a55a4c85863fcdf703aef41d
shape.shi <shape.shi@handeson.com>
2019-11-16 10:17:11 +0800

    x

--- my_frame/src/script/pipe.js
+++
@@ -1,8 +0,0 @@
-// 这个是从左往右的组合，compose是从右往左的组合
-export default const pipe = function (...args) {
-    return function (...a) {
-        return args.reduce(function (res, cb) {
-            return cb(res);
-        }, a);
-    }
-};
