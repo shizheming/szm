433dfacf2c966a3c7d8ada931ef6bab37568e92e
shape.shi <shi.zheming@towngas.com.cn>
2021-01-08 18:08:50 +0800

    添加vue3

---
+++ v3/src/App.vue
@@ -0,0 +1,41 @@
+<!--<template>
+  <div>q213123123123123</div>
+</template>-->
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// import { Button } from 'ant-design-vue';
+// import { h } from 'vue'
+export default {
+  name: 'App',
+  setup(){
+
+
+
+
+
+    console.log(1233333)
+    return () => {
+      return (
+        <ul>
+        <li>1111111111111</li>
+        <li>1111111111111</li>
+        <li>1111111111111</li>
+        <li><HelloWorld/></li>
+        </ul>
+        
+      )
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
