e7e25cbc1d5b620daa10cc3652d8150c74bdf6d2
DESKTOP-1FBD7EM\mingz <mingzheshi@126.com>
2021-01-21 23:56:34 +0800

    状态关系里面尝试最终输出dom来渲染

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,28 +2,82 @@
 import { bing } from "./relationship";
 export default {
   setup() {
-    let input = function(a, b) {
-      return a && <input disabled={b} />;
-    };
+    let ssss = [
+      {
+        be: "linkage",
+        name: "a",
+        relationship: [
+          {
+            be: "judge",
+            name: "b",
+            relationship: [
+              {
+                be: "state",
+                component: "input",
+                name: "inputState",
+                relationship: [
+                  {
+                    state: "show",
+                    relationship: {
+                      be: "judge",
+                      name: "j",
+                      relationship: [
+                        {
+                          name: "d",
+                        },
+                        {
+                          name: "e",
+                        },
+                      ],
+                    },
+                  },
+                  {
+                    state: "hide",
+                    relationship: {
+                      be: "linkage",
+                      name: "f",
+                      relationship: "d",
+                    },
+                  },
+                  {
+                    state: "able",
+                  },
+                  {
+                    state: "disabled",
+                  },
+                ],
+              },
+              {
+                name: "h",
+              },
+            ],
+          },
+          {
+            name: "i",
+          },
+        ],
+      },
+    ];
+
     let fnObj = {
       a() {
         console.log("a");
       },
       b() {
         console.log("b");
-        return "input";
+        return "inputState";
       },
       c() {
         console.log("c");
-        return 'e'
+        return "e";
       },
       d(v) {
         console.log("d");
         return v;
       },
       e(v) {
-        console.log("e");
-        return ["f", v];
+        console.log("e",v);
+        return v;
       },
       f(v) {
         console.log("f");
@@ -35,47 +89,68 @@ export default {
       h() {
         console.log("h");
       },
-      i() {
-        console.log("i");
+      i(v) {
+        console.log("i",v);
+        return v
       },
-      j() {
-        console.log("j");
+      j(v) {
+        console.log("j",v);
+        return ["e", v];
+      },
+      input(data) {
+        console.log(data, 20);
+        return select({
+          type: data,
+        });
+      },
+      inputState() {
+        console.log(1999);
+        return ["hide", "show", "disabled", "able"][displayName];
       },
     };
-
+    let displayName = 2;
+    
+    function select(data = {}) {
+      // console.log(hh,222111111111111111111111111)
+      let disabled = true;
+      const { type } = data;
+      let xxxxxxxxx = function() {
+        return (
+          <a-select
+            style={{ width: "40%" }}
+            placeholder="54545"
+            disabled={disabled}
+            options={[
+              {
+                label: 111,
+                value: 1,
+              },
+              {
+                label: 222,
+                value: 99,
+              },
+            ]}
+            {...data}
+          />
+        );
+      };
+      if (type === "show") {
+        console.log("show");
+        return xxxxxxxxx();
+      } else if (type === "disabled") {
+        console.log("disabled");
+        disabled = true;
+        return xxxxxxxxx();
+      } else if (type === "able") {
+        console.log("able");
+        disabled = false;
+        return xxxxxxxxx();
+      }
+    }
     return () => {
-      return (
-        <div>
-          <div id="nav">
-            <router-link to="/relationship">relationship</router-link> |
-            <router-link to="/linkage">linkage</router-link>
-          </div>
-          <router-view />
-        </div>
-      );
+      return <div>{bing(ssss, fnObj,2,3)}</div>;
     };
   },
 };
 </script>
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="less"></style>
--- vue3/src/relationship.js
+++ vue3/src/relationship.js
@@ -12,267 +12,117 @@
     2-隐藏
 */
 // 控件的状态几乎都是外部条件所决定的变化，很少有自己状态的变化，所以我先考虑前者
-/* const input = [
-  {
-    name: "input1",
-    condition: "aa",
-    state: "show",
-    fn: "a",
-  },
-  {
-    condition: "bb",
-    state: "hide",
-    fn: "b",
-  },
-  {
-    condition: "cc",
-    state: "able",
-    fn: "c",
-  },
-  {
-    condition: "dd",
-    state: "disabled",
-    fn: "d",
-  },
-]; */
-
-let ssss = [
-  {
-    be: "linkage",
-    name: "a",
-    relationship: [
-      {
-        be: "judge",
-        name: "b",
-        relationship: [
-          {
-            be: "state",
-            component: "input",
-            name: "inputState",
-            relationship: [
-              {
-                state: "show",
-                relationship: {
-                  be: "judge",
-                  name: "j",
-                  relationship: [
-                    {
-                      name: "d",
-                    },
-                    {
-                      name: "e",
-                    },
-                  ],
-                },
-              },
-              {
-                state: "hide",
-                relationship: {
-                  be: "linkage",
-                  name: "f",
-                  relationship: "d",
-                },
-              },
-              {
-                state: "able",
-              },
-              {
-                state: "disabled",
-              },
-            ],
-          },
-          {
-            name: "h",
-          },
-        ],
-      },
-      {
-        name: "i",
-      },
-    ],
-  },
-];
-
-// 如果没写就拿父级的be
-let hhhhhh = [
-  {
-    be: "linkage",
-    name: "a",
-    relationship: [
-      {
-        be: "judge",
-        name: "b",
-        relationship: [
-          {
-            name: "c",
-          },
-          {
-            name: "d",
-          },
-        ],
-      },
-      {
-        be: "linkage",
-        name: "e",
-        relationship: {
-          be: "judge",
-          name: "f",
-          relationship: [
-            {
-              name: "g",
-            },
-            {
-              name: "h",
-            },
-          ],
-        },
-      },
-      {
-        be: "linkage",
-        name: "i",
-        relationship: "j",
-      },
-    ],
-  },
-];
-
-let fnObj = {
-  a() {
-    console.log("a");
-  },
-  b() {
-    console.log("b");
-    return "inputState";
-  },
-  c() {
-    console.log("c");
-    return "e";
-  },
-  d(v) {
-    console.log("d");
-    return v;
-  },
-  e(v) {
-    console.log("e");
-    return ["f", v];
-  },
-  f(v) {
-    console.log("f");
-    return "g";
-  },
-  g() {
-    console.log("g");
-  },
-  h() {
-    console.log("h");
-  },
-  i() {
-    console.log("i");
-  },
-  j() {
-    console.log("j");
-    return "e";
-  },
-  input(data) {
-    console.log(data, 20);
-    return <input />;
-  },
-  inputState() {
-    console.log(1999);
-    return "hide";
-  },
-};
 
 import { isPlainObject, isArray, isString } from "lodash";
 
 // 联动
 const simpleLinkage = function(relationshipTable, obj, args) {
+  let lastResult;
   const { relationship } = relationshipTable;
 
-  const result = arrFn(obj[relationshipTable.name](args));
+  const result = arrFn(obj[relationshipTable.name](...args));
 
   if (isString(relationship)) {
     // 说明已经到底了
-    obj[relationship]();
+    lastResult = obj[relationship](...result);
   } else if (isPlainObject(relationship)) {
     // 是对象说明是唯一得一条路往下联系
     addBe(relationship, relationshipTable);
-    r(relationship, obj);
+    lastResult = r(relationship, obj, result);
   } else if (isArray(relationship)) {
+    let arrResult = result;
     // 是数组说明是分岔路
     relationship.forEach((item) => {
       addBe(item, relationshipTable);
-      r(item, obj);
+      arrResult = r(item, obj, arrResult);
     });
+    lastResult = arrResult;
   }
+  console.log(lastResult,200)
+  return arrFn(lastResult);
 };
 
 // 判断
 const simpleStrategy = function(relationshipTable, obj, args) {
+  let lastResult;
   const { relationship } = relationshipTable;
 
-  args = arrFn(obj[relationshipTable.name](args));
+  let result = arrFn(obj[relationshipTable.name](...args));
   if (isPlainObject(relationship)) {
     // 是对象说明是唯一得一条路往下联系
     addBe(relationship, relationshipTable);
-    r(relationship, obj);
+    lastResult = r(relationship, obj, result);
   } else if (isArray(relationship)) {
-    const firstName = args.shift();
+    let arrResult = result;
+    const firstName = result.shift();
     // 是数组说明是分岔路
     const findObj = relationship.filter(({ name }) => name === firstName);
     findObj.forEach((item) => {
       addBe(item, relationshipTable);
-      r(item, obj);
+      arrResult = r(item, obj, arrResult);
     });
+    lastResult = arrResult;
   }
+  console.log(lastResult,100)
+  return arrFn(lastResult);
 };
 
 // 状态
 const simpleState = function(relationshipTable, obj, args) {
+  let lastResult;
   const { relationship } = relationshipTable;
   // 状态判断函数
-  const result = obj[relationshipTable.name]();
-  const [state] = relationship.filter(({ state }) => state === result);
-// 渲染组件
-  obj[relationshipTable.component](displayState(state.state));
-  r(state.relationship ? state.relationship : {}, obj);
+  const result = arrFn(obj[relationshipTable.name](...args));
+  const firstName = result.shift();
+  const [state] = relationship.filter(({ state }) => state === firstName);
+  // 渲染组件
+  let dom = obj[relationshipTable.component](
+    displayState(state.state, ...result)
+  );
+  lastResult = r(state.relationship ? state.relationship : {}, obj, [dom]);
+  console.log(lastResult,300)
+  return arrFn(lastResult);
 };
 
 // 合体
-export const bing = function(table, obj) {
+export const bing = function(table, obj, ...args) {
+  let result = args;
   table.forEach((current) => {
-    r(current, obj);
+    result = r(current, obj, result);
   });
+  return result
 };
 
-function r(current, obj) {
-  let result;
+function r(current, obj, args) {
+  console.log(args,500,current)
+  let result = args;
+
 
   if (current.be === "linkage") {
-    result = simpleLinkage(current, obj);
+    result = simpleLinkage(current, obj, result);
   } else if (current.be === "judge") {
-    result = simpleStrategy(current, obj);
+    result = simpleStrategy(current, obj, result);
   } else if (current.be === "state") {
-    result = simpleState(current, obj);
+    result = simpleState(current, obj, result);
   }
+  console.log(result,400)
+  return result;
 }
 
-// 没有写be就给他加上父级的be
+// 没有写be就给他加上父级的be,同时要判断下面有没有realtionship，如果没有就不要加了
 const addBe = function(current, parent) {
-  if (!current.be) current.be = parent.be;
+  if (!current.be&&current.relationship) current.be = parent.be;
 };
 const arrFn = function(v) {
   return isArray(v) ? v : [v];
 };
 
 // 显示哪种状态
-const displayState = function (state) {
-  return ({
-    show:true,
-    hide:false,
-    able:true,
-    disabled:false
-  })[state]
-}
-
-bing(ssss, fnObj);
+const displayState = function(state) {
+  return {
+    show: "show",
+    hide: "hide",
+    able: "able",
+    disabled: "disabled",
+  }[state];
+};
