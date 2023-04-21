9b4709193efdbb4570a419cf0359d474125b0729
shape.shi <shi.zheming@towngas.com.cn>
2023-04-21 09:13:13 +0800

    xxxx

--- vue3/src/HelloWorld.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  首页
-</template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-}
-</script>
--- vue3/src/components/selectOption.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <SelectOption>
-    <slot />
-  </SelectOption>
-</template>
-<script setup>
-import { SelectOption } from "ant-design-vue";
-
-</script>
